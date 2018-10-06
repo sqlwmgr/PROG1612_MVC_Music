88de4f8fc156567e0e0f2161f6e1af41c8a917ae
sqlwmgr <43897836+sqlwmgr@users.noreply.github.com>
2018-10-06 14:23:27 -0400

    Initial Version
    This is the solution file posted on blackboard.

---
+++ solution_MVC_Music/solution_MVC_Music/Controllers/AlbumsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using solution_MVC_Music.Data;
+using solution_MVC_Music.Models;
+
+namespace solution_MVC_Music.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        private readonly MusicContext _context;
+
+        public AlbumsController(MusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Albums
+        public async Task<IActionResult> Index()
+        {
+            var musicContext = _context.Albums.Include(a => a.Genre);
+            return View(await musicContext.ToListAsync());
+        }
+
+        // GET: Albums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums
+                .Include(a => a.Genre)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // GET: Albums/Create
+        public IActionResult Create()
+        {
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name");
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,YearProduced,Price,GenreID")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(album);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name", album.GenreID);
+            return View(album);
+        }
+
+        // GET: Albums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name", album.GenreID);
+            return View(album);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,YearProduced,Price,GenreID")] Album album)
+        {
+            if (id != album.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(album);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(album.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name", album.GenreID);
+            return View(album);
+        }
+
+        // GET: Albums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = await _context.Albums
+                .Include(a => a.Genre)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var album = await _context.Albums.FindAsync(id);
+            _context.Albums.Remove(album);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlbumExists(int id)
+        {
+            return _context.Albums.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ solution_MVC_Music/solution_MVC_Music/Controllers/SongsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using solution_MVC_Music.Data;
+using solution_MVC_Music.Models;
+
+namespace solution_MVC_Music.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly MusicContext _context;
+
+        public SongsController(MusicContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Songs
+        public async Task<IActionResult> Index()
+        {
+            var musicContext = _context.Songs.Include(s => s.Album).Include(s => s.Genre);
+            return View(await musicContext.ToListAsync());
+        }
+
+        // GET: Songs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Songs
+                .Include(s => s.Album)
+                .Include(s => s.Genre)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return View(song);
+        }
+
+        // GET: Songs/Create
+        public IActionResult Create()
+        {
+            ViewData["AlbumID"] = new SelectList(_context.Albums, "ID", "Name");
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name");
+            return View();
+        }
+
+        // POST: Songs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,AlbumID,GenreID")] Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(song);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AlbumID"] = new SelectList(_context.Albums, "ID", "Name", song.AlbumID);
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name", song.GenreID);
+            return View(song);
+        }
+
+        // GET: Songs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Songs.FindAsync(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            ViewData["AlbumID"] = new SelectList(_context.Albums, "ID", "Name", song.AlbumID);
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name", song.GenreID);
+            return View(song);
+        }
+
+        // POST: Songs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,AlbumID,GenreID")] Song song)
+        {
+            if (id != song.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(song);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SongExists(song.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AlbumID"] = new SelectList(_context.Albums, "ID", "Name", song.AlbumID);
+            ViewData["GenreID"] = new SelectList(_context.Genres, "ID", "Name", song.GenreID);
+            return View(song);
+        }
+
+        // GET: Songs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Songs
+                .Include(s => s.Album)
+                .Include(s => s.Genre)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return View(song);
+        }
+
+        // POST: Songs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var song = await _context.Songs.FindAsync(id);
+            _context.Songs.Remove(song);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SongExists(int id)
+        {
+            return _context.Songs.Any(e => e.ID == id);
+        }
+    }
+}
